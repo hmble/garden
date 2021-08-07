4ba8b7a4a492e57e654904ccacedabfbd4bb1432
Samyak Bakliwal <w3bcode@gmail.com>
2021-08-07 15:26:48 +0530

    initial jit setup

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  mode: "jit",
+  purge: ["./content/**/*.md", "./content/**/*.html", "./layouts/**/*.html"],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+};
