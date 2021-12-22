267341db1b504e31d1e4caf28d31dd65ad474756
Samyak Bakliwal <w3bcode@gmail.com>
2021-12-22 10:34:30 +0530

    fix: update code to latest tailwindcss

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,3 @@
-const colors = require("tailwindcss/colors");
-const typography = require("@tailwindcss/typography");
 module.exports = {
   content: ["./content/**/*.md", "./content/**/*.html", "./layouts/**/*.html"],
   darkMode: "class", // or 'media' or 'class'
@@ -12,50 +10,13 @@ module.exports = {
       colors: {
         darkgray: "#151515",
       },
-      typography: {
-        DEFAULT: {
-          css: {
-            "*,*::before,*::after": {
-              color: colors.black,
-            },
-            color: colors.black,
-            a: {
-              color: colors.purple["500"],
-              textDecoration: "none",
-              backgroundColor: "red.400",
-            },
-            pre: false,
-            code: false,
-            "pre code": false,
-            "code::before": false,
-            "code::after": false,
-          },
-        },
-        dark: {
-          css: {
-            "*,*::before,*::after": {
-              color: "#F4F6FF",
-            },
-            color: colors.white,
-            a: {
-              color: colors.purple["500"],
-              textDecoration: "none",
-            },
-            pre: false,
-            code: false,
-            "pre code": false,
-            "code::before": false,
-            "code::after": false,
-          },
-        },
-      },
     },
   },
   variants: {
     extend: {},
   },
   plugins: [
-    typography,
+    require("@tailwindcss/typography"),
     // ...
   ],
 };
